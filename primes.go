5ee931a1f19b458ce89acc23ef22bb56caa7c9dc
walton <bigw369@gmail.com>
2017-03-19 21:25:01 -0400

    makes file writing more efficient

--- primes.go
+++ primes.go
@@ -7,28 +7,26 @@ import (
 )
 
 func primes() {
-	const maxInt uint = 1000
+	os.Create("out.csv")
+	f, err := os.OpenFile("out.csv", os.O_RDWR|os.O_APPEND, 0660)
+	if err != nil {
+		fmt.Println(err)
+	}
+	writer := bufio.NewWriter(f)
+
+	const maxInt uint = 10000000
 	var nums [maxInt]bool
 	for i := uint(2); i < maxInt; i++ {
 		// false is unseen
 		if nums[i] == false {
-			writeToFile(i)
+			fmt.Fprintln(writer, i)
 			for j := i; j < maxInt; j += i {
 				nums[j] = true
 			}
 		}
 	}
-}
-
-func writeToFile(num uint) {
-	f, err := os.OpenFile("primes.csv", os.O_RDWR|os.O_APPEND, 0660)
-	if err != nil {
-		fmt.Println(err)
-	}
-	writer := bufio.NewWriter(f)
-	fmt.Fprintln(writer, num)
 	writer.Flush()
-	defer f.Close()
+	f.Close()
 }
 
 func main() {
