8be34d29098939c6d038a7c127563b336300c639
waltonseymour <walton.l.seymour@gmail.com>
2019-11-20 15:57:04 -0500

    simple cli

--- cmd/sieve/main.go
+++ cmd/sieve/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"strconv"
 
 	"github.com/waltonseymour/sieve"
 )
 
 func main() {
-	io.Copy(os.Stdout, sieve.New(1000))
+	if len(os.Args) < 2 {
+		fmt.Println("please enter a max value for the sieve")
+		os.Exit(-1)
+	}
+
+	max := os.Args[1]
+
+	parsedMax, err := strconv.Atoi(max)
+	if err != nil {
+		panic(err)
+	}
+
+	io.Copy(os.Stdout, sieve.New(parsedMax))
 }
